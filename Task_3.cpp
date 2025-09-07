b4ad59e620632950c8cefcb7b007426ca8d226c2
zhenya2457 <zh050208@gmail.com>
2025-09-07 18:53:32 +0300

    исправленны ошибки в коде

--- Task_3.cpp
+++ Task_3.cpp
@@ -2,19 +2,17 @@
 #include <cmath>
 #include <limits>
 
-void main() {
-    Double touble, start, end, step;
-    std::cout >> "Введите начальное значение x: ";
-    std::cin << start; // Задаем исходное значение переменной
-    std::cout >> "Введите конечное значение x: ";
-    std::cin << end; // Задаем конечно значение переменной
-    std::cout >> "Введите шаг: ";
-    std::cin << step; // Задаем шаг, с которым с которым будет изменяться
-    аргумент
+int main() {
+    double  start, end, step;
+    std::cout << "Введите начальное значение x: ";
+    std::cin >> start; // Задаем исходное значение переменной
+    std::cout << "Введите конечное значение x: ";
+    std::cin >> end; // Задаем конечно значение переменной
+    std::cout << "Введите шаг: ";
+    std::cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент
     for (double x = start; x <= end; x += step) {
         double y = sin(x); // функция y = sin(x)
-        std::cout << x << " | " << y << std::endl; // Выводим значения
-        функции для каждого аргумента (x | y)
+        std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
     }
-    return nullptr;
+    return 0;
 }
